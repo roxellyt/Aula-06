9da5294452da69a79b7ed86d62d04d91905013a1
roxellyt <roxelly.tavares@gmail.com>
2022-03-18 21:58:26 -0300

    1 file modified

--- src/app/juros/juros.component.ts
+++ src/app/juros/juros.component.ts
@@ -10,13 +10,14 @@ export class JurosComponent implements OnInit {
   c: number;
   j: number;
   p: number;
-  add: string;
+  mcjp: number;
 
   constructor() {
     this.m = 800;
     this.c = 1000;
     this.j = 1000;
     this.p = 9;
+    this.mcjp;
   }
 
   getJuros() {
